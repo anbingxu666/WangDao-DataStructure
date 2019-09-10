49afdf7383a046e3632ab26e753a7af7053fcfe2
anbingxu666 <anbingxu666@163.com>
2019-09-10 21:47:50 +0800

    去除注释大内容

--- 排序/选择排序.cpp
+++ 排序/选择排序.cpp
@@ -1,34 +1,34 @@
-////
-//// Created by 安炳旭 on 2019-04-11.
-////
 //
-//#include <cstdio>
+// Created by Newmor on 2019-04-11.
 //
-//void selectSort(int *a, int len) {
-//
-//    for (int i = 0; i < len - 1; ++i) {
-//        //选出最小的下标
-//        int minIndex = i;
-//        for(int j = i + 1;j < len;++j){
-//            if(a[minIndex] > a[j]){
-//                minIndex = j;
-//            }
-//
-//            //交换元素位置
-//            if(minIndex!= i){
-//                int temp = a[minIndex];
-//                a[minIndex] = a[i];
-//                a[i] = temp;
-//            }
-//        }
-//    }
-//}
-//int main() {
-//    int a[10] = {4, 3, 5, 1, 2, 6, 9, 10, 7, 8};
-//
-//    selectSort(a, 10);
-//    for (int i : a) {
-//        printf("%d\t", i);
-//    }
-//    return 0;
-//}
+
+#include <cstdio>
+
+void selectSort(int *a, int len) {
+
+    for (int i = 0; i < len - 1; ++i) {
+        //选出最小的下标
+        int minIndex = i;
+        for(int j = i + 1;j < len;++j){
+            if(a[minIndex] > a[j]){
+                minIndex = j;
+            }
+
+            //交换元素位置
+            if(minIndex!= i){
+                int temp = a[minIndex];
+                a[minIndex] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+}
+int main() {
+    int a[10] = {4, 3, 5, 1, 2, 6, 9, 10, 7, 8};
+
+    selectSort(a, 10);
+    for (int i : a) {
+        printf("%d\t", i);
+    }
+    return 0;
+}
